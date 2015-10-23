be8ce38488fafce5767488e988267e48c963ae51
William Roberts <william.c.roberts@intel.com>
2015-10-23 09:45:07 -0700

    libsepol: fix memory leak on ops
    Valgrind reports:
    
    ==17050== HEAP SUMMARY:
    ==17050==     in use at exit: 648 bytes in 18 blocks
    ==17050==   total heap usage: 15,026 allocs, 15,008 frees, 576,913 bytes allocated
    ==17050==
    ==17050== 648 bytes in 18 blocks are definitely lost in loss record 1 of 1
    ==17050==    at 0x4C2CC70: calloc (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
    ==17050==    by 0x117C4B: avtab_insert_node (avtab.c:105)
    ==17050==    by 0x117BC0: avtab_insert (avtab.c:163)
    ==17050==    by 0x1187DA: avtab_read_item (avtab.c:588)
    ==17050==    by 0x118BA3: avtab_read (avtab.c:622)
    ==17050==    by 0x125BAD: policydb_read (policyd
    
    Change-Id: I115c8d4da1c3aef1e6aa9e33c83cbc7a70ce1499
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- libsepol/src/avtab.c
+++ libsepol/src/avtab.c
@@ -310,6 +310,8 @@ void avtab_destroy(avtab_t * h)
 		while (cur != NULL) {
 			temp = cur;
 			cur = cur->next;
+			if (temp->key.specified & AVTAB_OP)
+			    free(temp->datum.ops);
 			free(temp);
 		}
 		h->htable[i] = NULL;
