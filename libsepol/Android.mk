d68fe70c607405777491ecbe8acbfbb645506fb5
bowgotsai <bowgotsai@google.com>
2016-07-11 14:20:54 +0800

    Add libsepol and secil host and device targets
    Also clean up some LOCAL_C_INCLUDES as it should be included
    by LOCAL_EXPORT_C_INCLUDE_DIRS from libsepol.
    
    BUG=31366888
    
    Change-Id: I0e21279097f0635761672b838ad26861fc49e9ea

--- libsepol/Android.mk
+++ libsepol/Android.mk
@@ -92,7 +92,9 @@ LOCAL_C_INCLUDES := $(common_includes)
 LOCAL_CFLAGS := $(common_cflags)
 LOCAL_CPPFLAGS := $(yacc_flags)
 LOCAL_SRC_FILES := $(common_src_files) $(cil_src_files)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(LOCAL_PATH)/include \
+    $(LOCAL_PATH)/cil/include
 
 include $(BUILD_HOST_SHARED_LIBRARY)
 
@@ -107,7 +109,9 @@ LOCAL_C_INCLUDES := $(common_includes)
 LOCAL_CFLAGS := $(common_cflags)
 LOCAL_CPPFLAGS := $(yacc_flags)
 LOCAL_SRC_FILES := $(common_src_files) $(cil_src_files)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(LOCAL_PATH)/include \
+    $(LOCAL_PATH)/cil/include
 
 include $(BUILD_HOST_STATIC_LIBRARY)
 
@@ -131,7 +135,10 @@ LOCAL_MODULE := libsepol
 LOCAL_MODULE_TAGES := optional
 LOCAL_C_INCLUDES := $(common_includes)
 LOCAL_CFLAGS := $(common_cflags)
-LOCAL_SRC_FILES := $(common_src_files)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_CPPFLAGS := $(yacc_flags)
+LOCAL_SRC_FILES := $(common_src_files) $(cil_src_files)
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(LOCAL_PATH)/include \
+    $(LOCAL_PATH)/cil/include
 
 include $(BUILD_STATIC_LIBRARY)
