a860af952db92ca754da463e8f7bddb02d6613f1
Bram Bonné <brambonne@google.com>
2020-10-16 12:05:37 +0200

    Add fuzz targets for SELinux.
    Test: m libselinux_android_load_policy_fuzzer
    Test: m libselinux_android_setcontext_fuzzer
    Test: m libselinux_context_fuzzer
    Change-Id: I82e82ff41b1d969e6756658955dbbdbb6330eb43

---
+++ libselinux/fuzzers/ContextFuzzer.cpp
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include <selinux/context.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, [[maybe_unused]] size_t size) {
+  context_new((char*) data);
+
+  return 0;
+}
