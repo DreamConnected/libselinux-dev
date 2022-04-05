58fcab2652ffd11668274072a8cfcfaaaa5b463b
sandrom <sandrom@google.com>
2022-04-05 08:47:12 +0000

    Add semergerc to merge binary selinux policies
    Test: manual
    Change-Id: Ib9d833ca9a6379545a62c320f68c1f039a9ec3e5

--- libsepol/src/policydb.c
+++ libsepol/src/policydb.c
@@ -773,6 +773,13 @@ static int roles_init(policydb_t * p)
 			   (p->policy_type ==
 			    POLICY_MOD ? SCOPE_REQ : SCOPE_DECL), 1,
 			   &role->s.value);
+	p->p_role_val_to_name = malloc(sizeof(char *));
+	if(p->p_role_val_to_name == NULL)
+		goto out_free_key;
+	p->p_role_val_to_name[0] = strdup(key);
+	if(p->p_role_val_to_name[0] ==NULL) {
+		goto out_free_key;
+	} 
 	if (rc)
 		goto out_free_key;
 	if (role->s.value != OBJECT_R_VAL) {
@@ -4196,6 +4203,9 @@ static sepol_access_vector_t policydb_string_to_av_perm(
 	return 0;
 }
 
+int policydb_validate(policydb_t *p, struct policy_file *fp){
+	return validate_policydb(fp->handle, p);
+}
 
 /*
  * Read the configuration data from a policy database binary
