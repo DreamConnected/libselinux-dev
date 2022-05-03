562da1b26ab50e5917695501436f64d684b7579e
sandrom <sandrom@google.com>
2022-05-03 14:25:21 +0000

    Add secombinerc
    Test: adb shell secombinerc -b <binary-policy> -i <additional-policy>.cil -o /dev/sepolicy.XXXXXX
    
    Change-Id: I104fb86305bbc3d4b9fb66be3059f9c7a9074ad0

--- libsepol/cil/src/android.c
+++ libsepol/cil/src/android.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "cil_binary.h"
 #include "cil_build_ast.h"
 #include "cil_internal.h"
 #include "cil_strpool.h"
@@ -930,4 +931,16 @@ int cil_android_attributize(struct cil_db *tgtdb, struct cil_db *srcdb, const ch
 exit:
 	ver_map_destroy(ver_map_tab);
 	return rc;
+}
+
+int cil_amend_policydb(cil_db_t *db, sepol_policydb_t *pdb) {
+  fprintf(stderr, "db->num_types_and_attrs=%d pdb->p.p_types.nprim=%d.\n",
+          db->num_types_and_attrs, pdb->p.p_types.nprim);
+  db->num_types_and_attrs += pdb->p.p_types.nprim;
+
+  fprintf(stderr, "db->num_classes=%d pdb->p.p_classes.nprim=%d.\n",
+          db->num_classes, pdb->p.p_classes.nprim);
+  db->num_classes += pdb->p.p_classes.nprim;
+
+  return cil_binary_create_allocated_pdb(db, pdb);
 }
