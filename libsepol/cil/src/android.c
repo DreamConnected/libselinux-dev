7f0d294a49ec2aba9012ad377e33e88bd77ab9f3
sandrom <sandrom@google.com>
2022-05-09 14:39:17 +0000

    Implement cil_amend_policydb in android.c
    Test: adb shell secombinerc -b <binary-policy> -i <additional-policy>.cil -o /dev/sepolicy.XXXXXX
    Change-Id: I7523c4461db0d6ea50affda6f01601987193c426

--- libsepol/cil/src/android.c
+++ libsepol/cil/src/android.c
@@ -934,12 +934,10 @@ exit:
 }
 
 int cil_amend_policydb(cil_db_t *db, sepol_policydb_t *pdb) {
-  fprintf(stderr, "db->num_types_and_attrs=%d pdb->p.p_types.nprim=%d.\n",
-          db->num_types_and_attrs, pdb->p.p_types.nprim);
-  db->num_types_and_attrs += pdb->p.p_types.nprim;
+  db->handle_unknown = pdb->p.handle_unknown;
+  db->mls = pdb->p.mls;
 
-  fprintf(stderr, "db->num_classes=%d pdb->p.p_classes.nprim=%d.\n",
-          db->num_classes, pdb->p.p_classes.nprim);
+  db->num_types_and_attrs += pdb->p.p_types.nprim;
   db->num_classes += pdb->p.p_classes.nprim;
 
   return cil_binary_create_allocated_pdb(db, pdb);
