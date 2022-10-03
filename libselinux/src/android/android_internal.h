2d218282d89486678aeb0a85b56fea7eda7fb4a1
Thiébaud Weksteen <tweek@google.com>
2022-10-03 15:13:49 +1100

    Define and use struct path_alts
    The contexts file locations are represented using a 2-dimension array of
    paths. Refactor the type to simplify the syntax.
    
    Bug: 234313751
    Test: m
    Change-Id: Iaf62955d1c142f4210215cecbee427e91031516e

--- libselinux/src/android/android_internal.h
+++ libselinux/src/android/android_internal.h
@@ -14,11 +14,15 @@ extern "C" {
 // The maximum number of alternatives for a file on one partition.
 #define MAX_ALT_CONTEXT_PATHS 2
 
+struct path_alts {
+	const char *const paths[MAX_CONTEXT_PATHS][MAX_ALT_CONTEXT_PATHS];
+};
+
 /* Within each set of files, adds the first file that is accessible to `paths`.
  * Returns the number of accessible files. */
 size_t find_existing_files(
-	const char* const path_sets[MAX_CONTEXT_PATHS][MAX_ALT_CONTEXT_PATHS],
-	const char* paths[MAX_CONTEXT_PATHS]);
+	const struct path_alts *path_sets,
+	const char *paths[MAX_CONTEXT_PATHS]);
 
 /* Converts an array of file paths into an array of options for selabel_open.
  * opts must be at least as large as paths. */
