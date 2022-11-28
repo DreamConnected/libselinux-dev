2e394eb385db5494e66f91f56cfcf6c4e5b618b3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2022-11-28 02:27:29 +0000

    Revert "Fix build after upstream merge"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/84b3e97a-686c-4f32-9bd6-b198d82ba447). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I671c64103ab319cb5ce47eeb806e2f3c1e17fa54

--- libselinux/src/android/android_device.c
+++ libselinux/src/android/android_device.c
@@ -56,7 +56,7 @@ int selinux_android_context_with_level(const char * context,
 		goto out;
 	}
 
-	const char * newString = context_str(ctx);
+	char * newString = context_str(ctx);
 	if (!newString) {
 		goto out;
 	}
@@ -92,19 +92,19 @@ int selinux_android_setcontext(uid_t uid,
 			       const char *seinfo,
 			       const char *pkgname)
 {
-	char *orig_ctx_str = NULL;
-	const char *ctx_str = NULL;
+	char *orig_ctx_str = NULL, *ctx_str;
 	context_t ctx = NULL;
 	int rc = -1;
 
 	if (is_selinux_enabled() <= 0)
 		return 0;
 
-	rc = getcon(&orig_ctx_str);
+	rc = getcon(&ctx_str);
 	if (rc)
 		goto err;
 
-	ctx = context_new(orig_ctx_str);
+	ctx = context_new(ctx_str);
+	orig_ctx_str = ctx_str;
 	if (!ctx)
 		goto oom;
 
@@ -432,8 +432,7 @@ static int pkgdir_selabel_lookup(const char *pathname,
 {
     char *pkgname = NULL;
     struct pkg_info *info = NULL;
-    const char *orig_ctx_str = *secontextp;
-    const char *ctx_str = NULL;
+    char *secontext = *secontextp;
     context_t ctx = NULL;
     int rc = 0;
     unsigned int userid_from_path = 0;
@@ -459,7 +458,7 @@ static int pkgdir_selabel_lookup(const char *pathname,
         info->uid += userid_from_path * AID_USER_OFFSET;
     }
 
-    ctx = context_new(orig_ctx_str);
+    ctx = context_new(secontext);
     if (!ctx)
         goto err;
 
@@ -468,19 +467,19 @@ static int pkgdir_selabel_lookup(const char *pathname,
     if (rc < 0)
         goto err;
 
-    ctx_str = context_str(ctx);
-    if (!ctx_str)
+    secontext = context_str(ctx);
+    if (!secontext)
         goto err;
 
-    if (!strcmp(ctx_str, orig_ctx_str))
+    if (!strcmp(secontext, *secontextp))
         goto out;
 
-    rc = security_check_context(ctx_str);
+    rc = security_check_context(secontext);
     if (rc < 0)
         goto err;
 
     freecon(*secontextp);
-    *secontextp = strdup(ctx_str);
+    *secontextp = strdup(secontext);
     if (!(*secontextp))
         goto err;
 
