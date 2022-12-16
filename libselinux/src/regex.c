462e2aeec0a74242b17d324f8f3698b96db6a038
Inseob Kim <inseob@google.com>
2022-12-16 11:10:37 +0900

    Workaround for heap overhead of pcre
    pcre's behavior is changed so that pcre2_match always allocates heap for
    match_data, rather than using stacks for some cases. The heap isn't
    freed until explicitly calling pcre2_match_data_free, resulting in heap
    overhead.
    
    To workaround it, free and reallocate match_data whenever we call
    regex_match. Based on the boot time, this doesn't seem to affect the
    running time.
    
    Bug: 262469329
    Test: atest MicrodroidBenchmarks
    Change-Id: I9250e6aa31ac2b9a181af03ea5b47d57b1ae668c

--- libselinux/src/regex.c
+++ libselinux/src/regex.c
@@ -217,6 +217,12 @@ int regex_match(struct regex_data *regex, char const *subject, int partial)
 	rc = pcre2_match(
 	    regex->regex, (PCRE2_SPTR)subject, PCRE2_ZERO_TERMINATED, 0,
 	    partial ? PCRE2_PARTIAL_SOFT : 0, regex->match_data, NULL);
+	// ANDROID: pcre2_match allocates heap and it won't be freed until
+	// pcre2_match_data_free, resulting in heap overhead.
+	// Reallocate match_data to prevent such overhead
+	pcre2_match_data_free(regex->match_data);
+	regex->match_data = pcre2_match_data_create_from_pattern(
+	    regex->regex, NULL);
 	__pthread_mutex_unlock(&regex->match_mutex);
 	if (rc > 0)
 		return REGEX_MATCH;
